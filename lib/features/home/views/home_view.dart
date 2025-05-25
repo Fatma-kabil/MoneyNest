d2f1f287408cdb7faa4edf327c08aff47698fa66
Fatma_kabil <fatmakhaled112@gmail.com>
2025-05-25 08:53:36 +0200

    add floating action button

--- lib/features/home/views/home_view.dart
+++ lib/features/home/views/home_view.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -15,7 +17,7 @@ class HomeView extends StatelessWidget {
           showSelectedLabels: false,
           showUnselectedLabels: false,
           elevation: 3,
-          
+
           items: [
             BottomNavigationBarItem(
               icon: Icon(CupertinoIcons.home),
@@ -28,7 +30,32 @@ class HomeView extends StatelessWidget {
           ],
         ),
       ),
-      
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        // elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16), // تحكّمي في الزوايا هنا
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16), // نفس الزوايا هنا
+            gradient: LinearGradient(
+              colors: [
+                Theme.of(context).colorScheme.primary,
+                Theme.of(context).colorScheme.secondary,
+                Theme.of(context).colorScheme.tertiary,
+              ],
+              transform: GradientRotation(pi / 4),
+            ),
+          ),
+          child: const SizedBox(
+            width: 60, // عدّلي الحجم زي ما تحبي
+            height: 60,
+            child: Icon(CupertinoIcons.add),
+          ),
+        ),
+      ),
     );
   }
 }
