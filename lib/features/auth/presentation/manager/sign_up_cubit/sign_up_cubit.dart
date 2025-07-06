e5b61c87f40d30784e71b8cc70670f1823711239
Fatma_kabil <fatmakhaled112@gmail.com>
2025-07-06 10:09:53 +0200

    create sign up cubit

---
+++ lib/features/auth/presentation/manager/sign_up_cubit/sign_up_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:money_nest/features/auth/domain/entites/sign_up_enitity.dart';
+import 'package:money_nest/features/auth/domain/repos/auth_repo.dart';
+
+part 'sign_up_state.dart';
+
+class SignUpCubit extends Cubit<SignUpState> {
+  final AuthRepo authRepo;
+  SignUpCubit(this.authRepo) : super(SignUpInitial());
+
+  Future<void> signUp(SignUpEnitity user) async {
+    emit(SignUpLoading());
+    try {
+      await authRepo.signUp(user);
+      emit(SignUpSuccess());
+    } catch (e) {
+      emit(SignUpFailure(e.toString()));
+    }
+  }
+}
